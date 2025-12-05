f2769932eab760688e7ecc1c25bad63ea087176f
vpenalosa <vmpenalosa@gmail.com>
2025-12-05 14:53:00 -0500

    basic chatbox component no user

--- my-react-app/src/server.js
+++ my-react-app/src/server.js
@@ -5,6 +5,8 @@ const app = express();
 import session from 'express-session';
 import configRoutesFunction from './routes/index.js';
 import cors from 'cors';
+import { Server } from "socket.io"
+import { createServer } from "http"
 //import * as flat from 'flat';
 
 
@@ -34,8 +36,30 @@ app.use(
 
 configRoutesFunction(app);
 
+//create an http server instead of app.listen so socketio can live on the same server as express
+const httpServer = createServer(app);
 
-app.listen(3000, () => {
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:8080", "null"],
+    credentials: true,
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log("New socket connected: ", socket.id);
+
+  socket.on("chatMessage", (msg) => {
+    console.log("Received char: ", msg);
+    io.emit("chatMessage", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
+  });
+})
+
+httpServer.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log('Your routes + websocket will be running on http://localhost:3000');
 });
