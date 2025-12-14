5b4c3e96a0f5e29de7605577812b3ab96c985a88
Thotatb <tanavthota@gmail.com>
2025-12-14 13:41:28 -0500

    passwordhash

--- my-react-app/src/routes/accountData.js
+++ my-react-app/src/routes/accountData.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import {accounts} from './mongo/MongoCollections.js';
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
 
 const exportedMethods = {
     async login(username, password){
         const accountsCollection = await accounts();
         const user = await accountsCollection.findOne({ username });
-        if (!user) throw "User not found";
-
+        if (!user) throw "Either the username or password is invalid";
+        
         //password check;
+        let passwordCompare = false;
+        passwordCompare = await bcrypt.compare(password, user.password)
+        if (!passwordCompare) throw "Either the username or password is invalid"
         console.log(user);
         return user;
         return { id: user._id, username: user.username };
@@ -25,11 +30,11 @@ const exportedMethods = {
         const todayString = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
         console.log(todayString); 
 
-
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         //const passwordHash = await bcrypt.hash(password, 12);
         const newUser = { 
             username, 
-            password, 
+            hashedPassword, 
             winrates: {
                 chessWins: 0,
                 chessLosses: 0,
@@ -39,6 +44,12 @@ const exportedMethods = {
                 connectLosses: 0,
                 maniaWins: 0,
                 maniaLosses: 0
+            },
+            elo: {
+                chess: 800,
+                checkers: 800,
+                connect: 800,
+                mania: 800
             },
             signupDate: todayString
 
