f289e5069dd8a7916f26edd7575e39a3d9bc3f2c
Thotatb <tanavthota@gmail.com>
2025-12-16 21:13:37 -0500

    mmm yes

--- my-react-app/server.js
+++ my-react-app/server.js
@@ -28,9 +28,10 @@ app2.use(express.urlencoded({ extended: true }));
 
 // Allow us to send requests from react to here
 app2.use(cors({
-  origin: process.env.FRONTEND_CLIENT,
+  origin: 'https://testing-game-1tbcss.web.app',
   credentials: true
 }));
+app2.options('*', cors());
 
 app2.use(session({
   name: 'AwesomeWebapp2',
